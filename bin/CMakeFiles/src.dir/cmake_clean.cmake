b88a882991fe5ba34d79e9e88bf51543ba0ac10a
HyperGod <hypergod@hyperbolicbubble.com>
2012-12-01 12:05:33 -0500

    Initate

---
+++ bin/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/src.dir/main.cpp.obj"
+  "src.pdb"
+  "src.exe"
+  "src.exe.manifest"
+  "libsrc.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
